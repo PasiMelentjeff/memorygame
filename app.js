01f6e7bf3e8534ec8a5a492790f20079ebd7f40e
PasiMelentjeff <pasi.melentjeff@edu.savonia.fi>
2024-02-27 00:28:12 +0200

    lista valmis

---
+++ app.js
@@ -0,0 +1,95 @@
+const cardArray =[
+
+{
+    name: 'fries',
+    img: 'images/fries.png'
+},
+
+{
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+},
+
+{
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+},
+
+{
+
+name: 'hotdog',
+img: 'images/hotdog.png'
+
+
+},
+
+{
+
+    name: 'ice-cream',
+    img: 'images/ice-cream.png'
+    
+    
+    },
+    {
+
+        name: 'milkshake',
+        img: 'images/milshake.png'
+        
+        
+        },
+
+        {
+
+            name: 'pizza',
+            img: 'images/pizza.png'
+            
+            
+            },
+
+            {
+                name: 'fries',
+                img: 'images/fries.png'
+            },
+            
+            {
+                name: 'cheeseburger',
+                img: 'images/cheeseburger.png'
+            },
+            
+            {
+                name: 'cheeseburger',
+                img: 'images/cheeseburger.png'
+            },
+            
+            {
+            
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+            
+            
+            },
+            
+            {
+            
+                name: 'ice-cream',
+                img: 'images/ice-cream.png'
+                
+                
+                },
+                {
+            
+                    name: 'milkshake',
+                    img: 'images/milshake.png'
+                    
+                    
+                    },
+            
+                    {
+            
+                        name: 'pizza',
+                        img: 'images/pizza.png'
+                        
+                        
+                        }
+
+]
