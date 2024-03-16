808b0095faa8c9ccf55184292c7f72d20d656164
PasiMelentjeff <pasi.melentjeff@edu.savonia.fi>
2024-03-16 15:00:46 +0200

    Viivettä pitäisi saada toisen kuvan klikkauksessa muuten valmis

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const cardArray =[
     img: 'images/cheeseburger.png'
 },
 
-{
-    name: 'cheeseburger',
-    img: 'images/cheeseburger.png'
-},
+
 
 {
 
@@ -56,10 +53,7 @@ img: 'images/hotdog.png'
                 img: 'images/cheeseburger.png'
             },
             
-            {
-                name: 'cheeseburger',
-                img: 'images/cheeseburger.png'
-            },
+           
             
             {
             
@@ -93,3 +87,159 @@ img: 'images/hotdog.png'
                         }
 
 ]
+
+cardArray.sort(()=>0.5-Math.random())
+
+const gridDisplay=document.querySelector('#grid')
+
+let scoreDisplay=document.getElementById('result')
+
+let j=0
+
+let maara=0
+
+let nimi11=''
+
+const cardsShosen=[]
+
+let blankId=[]
+
+function createBoard(){
+
+
+for (i=0;i<cardArray.length;i++){
+
+
+const card=document.createElement('img')
+
+card.setAttribute('src','images/blank.png')
+
+card.setAttribute('data-id', i)
+
+card.addEventListener('click', flipCard)
+
+gridDisplay.appendChild(card)
+
+
+
+}
+
+
+
+
+
+}
+
+
+createBoard()
+
+
+function flipCard(){
+
+if (maara<2){
+
+
+maara++
+
+
+  const  cardId =this.getAttribute('data-id')
+
+ console.log(cardArray[cardId])
+
+console.log('klikattu',cardId)
+
+cardsShosen.push(cardArray[cardId].name)
+
+console.log(cardsShosen)
+
+
+
+
+
+
+if(cardsShosen.length===1){
+
+ nimi11 =cardsShosen[0]
+
+blankId[0]=cardId
+
+}
+
+if(cardsShosen.length===2){
+
+     nimi11 =cardsShosen[1]
+
+     blankId[1]=cardId
+    }
+
+
+let valinta='images/'
+
+let lahde=valinta+nimi11+'.png'
+
+this.setAttribute('src',lahde)
+
+
+if (cardsShosen.length>1){
+
+let nimi1=cardsShosen[0]
+
+let nimi2 =cardsShosen[1]
+
+if(nimi1===nimi2){
+
+j++
+
+scoreDisplay.innerHTML=j
+
+cardsShosen.length=0
+
+maara=0
+
+
+}
+else{
+
+
+
+ let xx =document.querySelectorAll('img')
+
+
+
+for (let i=0;i<blankId.length;i++){
+
+
+
+let numero2=blankId[i]
+
+
+for (let j=0;j<xx.length;j++){
+let kuva=xx[j]
+
+let numero=kuva.getAttribute('data-id')
+
+if(numero===numero2){
+
+
+kuva.setAttribute('src','images/blank.png')
+
+}
+}
+
+
+}
+
+    maara=0
+
+    cardsShosen.length=0
+
+
+}
+
+}
+
+ 
+
+}
+
+}
